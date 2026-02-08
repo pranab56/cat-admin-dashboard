aa4d8652241806e5514ca2041f1238a592e7199a
pranab56 <pronabhalder53@gmail.com>
2026-02-08 11:05:41 +0600

    new base url

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,1 +1,2 @@
-export const baseURL = "https://humayon5004.binarybards.online";
+// export const baseURL = "https://humayon5004.binarybards.online";
+export const baseURL = "http://34.200.16.104:5000";
